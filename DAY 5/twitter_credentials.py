3924322fad4acb0c6d104105184a72008b9f416e
kira <srivarsha.11@gmail.com>
2020-06-07 19:47:43 +0530

    Day 5 work

---
+++ DAY 5/twitter_credentials.py
@@ -0,0 +1,8 @@
+#variables that contain user credentials to access twitter api
+ACCESS_TOKEN="2835705882-08os91GULDem5IUSwF7QZQybizMRM2J1idZcsyA"
+
+ACCESS_SECRET="10JBgF5lAgy0jNOYd1aLmYG5OxBBPmZf0nf9LVdTlz759"
+
+CONSUMER_KEY="KJVUwIEWrpvY0rbugtqBTgCKC"
+
+CONSUMER_SECRET="YnrGVxrkMoeLGzE3gfHymwu8uZefNA4g8rG3wtmS3z9EwSsBUK"
